ea19e5cd2f0ab4438701115f06d1538711a9c98f
Howard L Frederick <simbamangu@gmail.com>
2018-08-30 12:39:53 +0300

    Updated R scripts and PiNotes

--- _r/Oblique GSD.R
+++ _r/Oblique GSD.R
@@ -16,16 +16,16 @@ phi <- 45 * pi / 180 # Inclination angle in radians from vertical (0°)
 
 gsdOblique <- function(Sw, f, Sh, phi, h, Pw) {
   ## Angle of view ----
-  a.h <- 2 * atan((Sw / 2) / f) # horizonal angle of view°
-  a.v <- 2 * atan((Sh / 2) / f) # vertical angle of view°
+  A.h <- 2 * atan((Sw / 2) / f) # horizonal angle of view°
+  A.v <- 2 * atan((Sh / 2) / f) # vertical angle of view°
   
   ## Strip width ----
   # w.1 - From zero (under aircraft) to inside edge of image
   # w.2 - From zero to outside edge
   # w.0 - width of image on ground
   
-  w.1 <- tan((phi - (a.v / 2))) * h 
-  w.2 <- tan((phi + (a.v / 2))) * h
+  w.1 <- tan((phi - (A.v / 2))) * h 
+  w.2 <- tan((phi + (A.v / 2))) * h
   w.0 <- w.2 - w.1
   
   ## GSD at various angles ----
@@ -38,11 +38,29 @@ gsdOblique <- function(Sw, f, Sh, phi, h, Pw) {
   h.1 <- sqrt(h^2 + w.1^2)
   h.2 <- sqrt(h^2 + w.2^2)
   
-  v.1 <- 2 * (tan(a.h/2)) * h.1 # inner frame width
-  v.2 <- 2 * (tan(a.h/2)) * h.2 # outer frame width
+  v.1 <- 2 * (tan(A.h/2)) * h.1 # inner frame width
+  v.2 <- 2 * (tan(A.h/2)) * h.2 # outer frame width
   
   gsd.1 <- v.1 / Pw # GSD lower edge of image
   gsd.2 <- v.2 / Pw # GDS upper edge of image
   
   return(data.frame(gsd.inner = gsd.1, gsd.outer = gsd.2))
+}
+
+gsdOblique2 <- function(Sw = 23.2, f = 35, Sh = 15.4, phi = 45 * pi/180, h = 450/3.28, Pw = 6016) {
+  ## Angle of view ----
+  A.h <- 2 * atan((Sw / 2) / f) # horizonal angle of view°
+  A.v <- 2 * atan((Sh / 2) / f) # vertical angle of view°
+  
+  h.1 <- h / cos(phi - A.v / 2) # length to lower edge of view
+  w.1 <- h.1 * Sw / f
+  h.2 <- h / cos(phi + A.v / 2)
+  w.2 <- h.2 * Sw / f
+  
+  gsd.1 <- w.1 / Pw
+  gsd.2 <- w.2 / Pw
+  
+  stripwidth <- sqrt(h^2 + h.2^2) - sqrt(h^2 + h.1^2)
+  
+  return(data.frame(gsd.inner = gsd.1, gsd.outer = gsd.2, width.inner = w.1, width.outer = w.2, stripwidth))
 }
